9dfd322c1f1c6515f546a7ce6a451129c1e79161
Shikha Gupta <shikhagupta@shikhagupta-macbookpro.roam.corp.google.com>
2019-04-29 11:54:39 +0530

    Updates

---
+++ precache-manifest.0dbfcaf6bf5eac2f2867f469d69b47ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71a404e05b6e3a8aea9dba24ace8a5d",
+    "url": "/.github.io/campaign-new/index.html"
+  },
+  {
+    "revision": "cce1e244bf3bc80507d6",
+    "url": "/.github.io/campaign-new/static/css/2.c5d966c8.chunk.css"
+  },
+  {
+    "revision": "e3741ffb6144177689ea",
+    "url": "/.github.io/campaign-new/static/css/main.896b492f.chunk.css"
+  },
+  {
+    "revision": "cce1e244bf3bc80507d6",
+    "url": "/.github.io/campaign-new/static/js/2.d2dcd681.chunk.js"
+  },
+  {
+    "revision": "e3741ffb6144177689ea",
+    "url": "/.github.io/campaign-new/static/js/main.b2f7b0ab.chunk.js"
+  },
+  {
+    "revision": "31669ab695298272a6b0",
+    "url": "/.github.io/campaign-new/static/js/runtime~main.20f34e52.js"
+  }
+]);
